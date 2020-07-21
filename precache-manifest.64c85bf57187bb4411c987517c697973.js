2786cb61f672096afd54797e46c633dd5e382170
blossun <dnsngu@gmail.com>
2020-07-21 14:06:24 +0900

    Updates

---
+++ precache-manifest.64c85bf57187bb4411c987517c697973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc965ad2e06fb4669374373d5d659a4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0518908df4545c9c749f",
+    "url": "/movie_app/static/css/main.fb70b584.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0518908df4545c9c749f",
+    "url": "/movie_app/static/js/main.2c2e5cac.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
